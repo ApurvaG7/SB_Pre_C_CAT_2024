dab26422c48675a676b72441bf1260034a5f4746
PH27_Apurva_83486 <57842054+ApurvaG7@users.noreply.github.com>
2024-03-20 18:58:44 +0530

    Assignment

---
+++ C Programming/Assignments/Ass1/q2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("the number %d in char will be %c",a,a);
+    
+}
---
+++ C Programming/Assignments/Ass1/q4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    printf("star pattern \n \n \n");
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    printf("\n\n\n\n");
+    printf("*\n**\n***\n****\n*****");
+
+}
